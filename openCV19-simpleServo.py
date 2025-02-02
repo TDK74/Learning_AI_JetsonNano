9b90b0273af0c6a4899f9e5ef82056810afc3058
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-02-02 13:11:51 +0200

    Added a Python file from Lesson 31.

---
+++ openCV19-simpleServo.py
@@ -0,0 +1,19 @@
+from adafruit_servokit import ServoKit
+import time
+
+
+myKit = ServoKit(channels = 16)
+
+while True:
+    for i in range(0, 180, 1):
+        myKit.servo[0].angle = i
+        myKit.servo[1].angle = i
+        time.sleep(0.01)
+
+    for i in range(180, 0, -1):
+        myKit.servo[0].angle = i
+        myKit.servo[1].angle = i
+        time.sleep(0.01)
+
+    if KeyboardInterrupt is True:
+        break
